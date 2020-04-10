43c44fbce14e7d433c9d5a040bff4c02dfd12881
kaori kohama <kkaori131415@gmail.com>
2020-04-10 15:10:40 +0200

    responsive pictures for property show page

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
    {
      breakpoint: 1025, // 768~1023px以下のサイズに適用
         settings: {
-           slidesToShow:3
+           slidesToShow:2
         }
     }, {
       breakpoint: 769, // 480〜767px以下のサイズに適用
@@ -26,4 +26,39 @@ $(document).ready(function(){
     }
  ]
   });
+  $('.center').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.center-nav'
+});
+$('.center-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.center',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+
+  responsive: [
+   {
+     breakpoint: 1025, // 768~1023px以下のサイズに適用
+        settings: {
+           slidesToShow:2
+        }
+    }, {
+      breakpoint: 769, // 480〜767px以下のサイズに適用
+        settings: {
+           slidesToShow: 2
+         }
+     },{
+ breakpoint: 480, // 〜479px以下のサイズに適用
+       settings: {
+          slidesToShow: 1
+       }
+    }
+ ]
+});
 })
+
