aa8bd4f390ce2d7583f80c870d369fbedf4c52cb
kaori kohama <kkaori131415@gmail.com>
2020-04-17 15:45:11 +0200

    added modal to contact agent

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,7 @@
+$(function(){
+
+  $('#contactAgent').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var modal = $(this)
+  });
+});
