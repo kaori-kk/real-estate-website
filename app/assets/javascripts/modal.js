4bf87fb2d848ce0d97063c0445f5c834aaef2d34
kaori kohama <kkaori131415@gmail.com>
2020-04-20 11:24:18 +0200

    ajax call for contact to agent

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,32 @@
 $(function(){
+  $("#send-message").on("click", function(){
+
+    const agent_id = $("#agent_id").val();
+    const first_name = $("#message-first-name").val();
+    const last_name = $("#message-last-name").val();
+    const email = $("#message-email").val();
+    const message = $("#message-text").val();
+
+  $.ajax({
+    url: "/agent/message",
+    method: "POST",
+    dataType: "json",
+    data: {
+      agent_id: agent_id,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      message: message
+    },
+    success: function(data){
+      console.log(data);
+    }
+  })
+ });
 
   $('#contactAgent').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var modal = $(this)
   });
+
 });
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -72,6 +72,18 @@ class PropertiesController < ApplicationController
     end
   end
 
+  def email_agent
+    agent_id = params[:agent_id]
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    email = params[:email]
+    message = params[:message]
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_property
