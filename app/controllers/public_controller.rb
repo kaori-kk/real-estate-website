cfcdf586b25dca2d8681e57257530ed7f54e9e6d
kaori kohama <kkaori131415@gmail.com>
2020-04-20 14:40:01 +0200

    seperated dashboard for admin and user

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,11 @@
 class PublicController < ApplicationController
   def main
+
+    if account_signed_in?
+      path = current_account.admin? ? accounts_path : dashboard_path
+
+    end
+
     @properties = Property.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :properties
+  devise_for :accounts
+
+  get "/accounts" => "admin#accounts", as: :accounts
+
   get '/dashboard' => 'dashboard#index', as: :'dashboard'
   get '/profile/:id' => 'dashboard#profile', as: :'profile'
   post '/agent/message' => 'properties#email_agent', as: :email_agent
@@ -7,8 +13,6 @@ Rails.application.routes.draw do
   get 'dashboard/properties'
   get 'dashboard/reports'
 
-  resources :properties
-  devise_for :accounts
   root to: 'public#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
